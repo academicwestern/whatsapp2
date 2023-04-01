6053655a1810968e4c61c24328696346b2eb66f7
academicwestern <129611777+academicwestern@users.noreply.github.com>
2023-04-01 16:56:01 +0100

    Initial Commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "FYP": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "FYP": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/navigation/MainTabNavigator.js
@@ -0,0 +1,50 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import SettingsScreen from "../screens/SettingsScreen";
+
+import ChatsScreen from "../screens/ChatsScreen/ChatsScreen";
+import { Ionicons, Entypo } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const MainTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Chats"
+      screenOptions={{
+        tabBarStyle: { backgroundColor: "whitesmoke" },
+        headerStyle: { backgroundColor: "whitesmoke" },
+      }}
+    >
+      <Tab.Screen
+        name="Comhrá"
+        component={ChatsScreen}
+        options={({ navigation }) => ({
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-chatbubbles-sharp" size={size} color={color} />
+          ),
+          headerRight: () => (
+            <Entypo
+              onPress={() => navigation.navigate("Contacts")}
+              name="new-message"
+              size={18}
+              color={"royalblue"}
+              style={{ marginRight: 15 }}
+            />
+          ),
+        })}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default MainTabNavigator;
